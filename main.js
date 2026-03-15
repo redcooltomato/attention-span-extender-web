9ff696ec2141343c570feee88c22a6ab24b46b93
redcooltomato <redcooltomato@gmail.com>
2026-03-15 13:57:17 +0300

    little fix

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let variable = localStorage.getItem("variable") || "";
 let input = prompt(variable, variable);
 
 if (input !== null) {
-    localStorage.setItem("variable");
+    localStorage.setItem("variable", input);
 }
